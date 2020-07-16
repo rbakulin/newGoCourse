9a592a2c6673d86eb132e0a4b07fd8f98bdcbcf0
Бакулин Роман Евгеньевич <rebakulin@domclick.ru>
2020-07-16 15:55:29 +0300

    Add error hanling

--- homework-1/converter.go
+++ homework-1/converter.go
@@ -15,6 +15,10 @@ func main()  {
 	var rublesStr string
 	fmt.Println("Введите сумму в рублях")
 	fmt.Scanln(&rublesStr)
-	rublesInt, _ := strconv.Atoi(rublesStr)
+	rublesInt, err := strconv.Atoi(rublesStr)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		return
+	}
 	fmt.Printf("Вы получите %d $", convertToDollars(rublesInt))
 }
--- homework-1/triangleMath.go
+++ homework-1/triangleMath.go
@@ -13,15 +13,29 @@ func calcTriangleParams(cathetus1 float64, cathetus2 float64) (float64, float64,
 	return hypotenuse, perimeter, square
 }
 
+func checkErr(err error){
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		return
+	}
+}
+
 func main()  {
 	var cathetus1Str string
 	var cathetus2Str string
+
 	fmt.Println("Введите катет 1")
-	fmt.Scanln(&cathetus1Str)
+	_, scanErr := fmt.Scanln(&cathetus1Str)
+	checkErr(scanErr)
 	fmt.Println("Введите катет 2")
-	fmt.Scanln(&cathetus2Str)
-	cathetus1Int, _ := strconv.ParseFloat(cathetus1Str, 64)
-	cathetus2Int, _ := strconv.ParseFloat(cathetus2Str, 64)
+	_, scanErr = fmt.Scanln(&cathetus2Str)
+	checkErr(scanErr)
+
+	cathetus1Int, parseErr := strconv.ParseFloat(cathetus1Str, 64)
+	checkErr(parseErr)
+	cathetus2Int, parseErr := strconv.ParseFloat(cathetus2Str, 64)
+	checkErr(parseErr)
+
 	hypotenuse, perimeter, square := calcTriangleParams(cathetus1Int, cathetus2Int)
 	fmt.Printf("Гипотенуза: %g, Периметр: %g, Площадь: %g", hypotenuse, perimeter, square)
 }
