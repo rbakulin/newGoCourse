97c633e471d6bba3a55d47770611a920e39cf737
Бакулин Роман Евгеньевич <rebakulin@domclick.ru>
2020-07-23 15:00:57 +0300

    Refactor converter.go

--- homework-1/converter.go
+++ homework-1/converter.go
@@ -2,23 +2,22 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
-var exchangeRate = 70
+const exchangeRate = 70
 
 func convertToDollars(rubles int) int{
 	return rubles * exchangeRate
 }
 
 func main()  {
-	var rublesStr string
 	fmt.Println("Введите сумму в рублях")
-	fmt.Scanln(&rublesStr)
-	rublesInt, err := strconv.Atoi(rublesStr)
+	var rubles int
+	_, err := fmt.Scanln(&rubles)
 	if err != nil {
 		fmt.Printf("%v\n", err)
 		return
 	}
-	fmt.Printf("Вы получите %d $", convertToDollars(rublesInt))
+
+	fmt.Printf("Вы получите %d$", convertToDollars(rubles))
 }
