aa35cb6ffa0c39210a7a4dd2971b619db94b3859
Бакулин Роман Евгеньевич <rebakulin@domclick.ru>
2020-08-07 14:56:12 +0300

    Image

---
+++ homework-6/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/draw"
+	"image/png"
+	"log"
+	"os"
+)
+
+func main() {
+	green := color.RGBA{0, 255, 0, 255}
+	red := color.RGBA{200, 30, 30, 255}
+	rectImg := image.NewRGBA(image.Rect(0, 0, 200, 200))
+
+	draw.Draw(rectImg, rectImg.Bounds(), &image.Uniform{green}, image.ZP, draw.Src)
+	// Lines
+	for x := 0; x < 200; x++ {
+		y := 100
+		rectImg.Set(x, y, red)
+	}
+	for y := 0; y < 200; y++ {
+		x := 100
+		rectImg.Set(x, y, red)
+	}
+
+
+	file, err := os.Create("rectangle.png")
+	if err != nil {
+		log.Fatalf("Failed create file: %s", err)
+	}
+	defer file.Close()
+	png.Encode(file, rectImg)
+}
