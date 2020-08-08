6c2ecfe47d321279d900554a3a119d3e40c70610
Бакулин Роман Евгеньевич <rebakulin@domclick.ru>
2020-08-08 18:35:19 +0300

    HTTP-server

--- homework-6/main.go
+++ homework-6/main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"image"
 	"image/color"
 	"image/draw"
 	"image/png"
+	"io"
 	"log"
+	"net/http"
 	"os"
 )
+
+func hello(res http.ResponseWriter, req *http.Request) {
+	name := "World"
+	paramName := req.FormValue("name")
+	if paramName != "" {
+		name = req.FormValue("name")
+	}
+	res.Header().Set("Content-Type", "text/html")
+	io.WriteString(res,
+		fmt.Sprintf("<doctype html><html><head><title>Hello %s</title>" +
+			"</head><body>Hello %s!</body></html>", name, name))
+}
 
 func main() {
 	green := color.RGBA{0, 255, 0, 255}
@@ -25,11 +40,17 @@ func main() {
 		rectImg.Set(x, y, red)
 	}
 
-
 	file, err := os.Create("rectangle.png")
 	if err != nil {
 		log.Fatalf("Failed create file: %s", err)
 	}
 	defer file.Close()
 	png.Encode(file, rectImg)
+
+	// HTTP-server
+	fs := http.FileServer(http.Dir("homework-6/static"))
+	http.Handle("/", fs)
+	http.HandleFunc("/hello", hello)
+
+	http.ListenAndServe(":80", nil)
 }
